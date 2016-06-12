aec36e597f85f705fb8656bafd5715f787f6ae09
Alex Gaynor <alex.gaynor@gmail.com>
2016-06-11 21:02:12 -0400

    Write this conditional correctly

--- drivers/char/mem.c
+++ drivers/char/mem.c
@@ -797,12 +797,12 @@ static const struct memdev {
 #endif
 	 [5] = { "zero", 0666, &zero_fops, 0 },
 	 [7] = { "full", 0666, &full_fops, 0 },
-#if CONFIG_BSD_RANDOM
-	 [8] = { "random", 0666, &random_fops, 0 },
-	 [9] = { "urandom", 0666, &urandom_fops, 0 },
-#else
+#ifdef CONFIG_BSD_RANDOM
 	 [8] = { "random", 0666, &bsd_urandom_fops, 0 },
 	 [9] = { "urandom", 0666, &bsd_urandom_fops, 0 },
+#else
+	 [8] = { "random", 0666, &random_fops, 0 },
+	 [9] = { "urandom", 0666, &urandom_fops, 0 },
 #endif
 	 [13] = { "bsd_urandom", 0666, &bsd_urandom_fops, 0 },
 #ifdef CONFIG_PRINTK
